16025bccb35eb6483983027318720e84da8379fa
kiran-s-m <kiransm007@gmail.com>
2023-01-04 10:58:48 +0530

    To check git, added a new file in service layer

---
+++ src/main/java/com/footwear/shoemanagement/service/ExtraClassToCheckGit.java
@@ -0,0 +1,8 @@
+package com.footwear.shoemanagement.service;
+
+public class ExtraClassToCheckGit {
+public static void main(String[] args) {
+	
+	System.out.println("Created this class to check Git...");
+}
+}
